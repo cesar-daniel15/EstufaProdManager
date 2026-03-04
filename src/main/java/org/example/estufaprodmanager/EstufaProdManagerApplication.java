268402ce593420df3ad90aae494f1a35e4c85c57
cesar-daniel15 <cesardaniel.contacto@gmail.com>
2026-03-04 11:00:26 +0000

    Initial Commit

---
+++ src/main/java/org/example/estufaprodmanager/EstufaProdManagerApplication.java
@@ -0,0 +1,13 @@
+package org.example.estufaprodmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstufaProdManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EstufaProdManagerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/estufaprodmanager/EstufaProdManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.estufaprodmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstufaProdManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
