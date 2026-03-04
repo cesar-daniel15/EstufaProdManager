78c8d224156d36256430cbed54c703c17ab95b5a
Rúben Oliveira Benedito <162583130+RubenBenedito@users.noreply.github.com>
2026-03-04 12:15:13 +0000

    Criação tabela Clientes e Encomendas

---
+++ src/main/java/org/example/estufaprodmanager/models/Clientes.java
@@ -0,0 +1,58 @@
+package org.example.estufaprodmanager.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import lombok.experimental.SuperBuilder;
+
+import java.io.Serializable;
+import java.util.Date;
+
+@Entity
+@Getter
+@Setter
+@ToString
+@SuperBuilder
+@NoArgsConstructor
+@Table(name = "clientes")
+public class Clientes implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @Column(name = "id", nullable = false)
+    private String id;
+
+    @Column(name = "id_utilizador", nullable = false)
+    private String idUtilizador;
+
+    @Column(name = "nif", nullable = false)
+    private String nif;
+
+    @Column(name = "setor_atividade")
+    private String setorAtividade;
+
+    @Column(name = "morada")
+    private String morada;
+
+    @Column(name = "codigo_postal")
+    private String codigoPostal;
+
+    @Column(name = "contacto")
+    private String contacto;
+
+    @Column(name = "criado_por")
+    private String criadoPor;
+
+    @Column(name = "data_criacao", nullable = false)
+    private Date dataCriacao;
+
+    @Column(name = "data_atualizacao")
+    private Date dataAtualizacao;
+
+}
---
+++ src/main/java/org/example/estufaprodmanager/models/Encomendas.java
@@ -0,0 +1,53 @@
+package org.example.estufaprodmanager.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import lombok.experimental.SuperBuilder;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.Date;
+
+@Entity
+@Getter
+@Setter
+@ToString
+@SuperBuilder
+@NoArgsConstructor
+@Table(name = "encomendas")
+public class Encomendas implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @Column(name = "id", nullable = false)
+    private String id;
+
+    @Column(name = "id_cliente", nullable = false)
+    private String idCliente;
+
+    @Column(name = "data_final")
+    private Date dataFinal;
+
+    @Column(name = "iva_total", nullable = false)
+    private BigDecimal ivaTotal;
+
+    @Column(name = "desconto_total", nullable = false)
+    private BigDecimal descontoTotal;
+
+    @Column(name = "estado_encomenda", nullable = false)
+    private String estadoEncomenda;
+
+    @Column(name = "data_criacao", nullable = false)
+    private Date dataCriacao;
+
+    @Column(name = "data_atualizacao")
+    private Date dataAtualizacao;
+
+}
