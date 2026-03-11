19635adfec97d102258db2bcf160334c67d4b9d2
cesar-daniel15 <cesardaniel.contacto@gmail.com>
2026-03-11 10:01:33 +0000

    Create Folder services

--- src/main/java/org/example/estufaprodmanager/services/FuncionalidadesService.java
+++ src/main/java/org/example/estufaprodmanager/services/FuncionalidadesService.java
@@ -1,4 +1,37 @@
 package org.example.estufaprodmanager.services;
 
+import org.example.estufaprodmanager.models.Funcionalidades;
+import org.example.estufaprodmanager.repositories.FuncionalidadesRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.swing.text.html.Option;
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class FuncionalidadesService {
+
+    @Autowired
+    private FuncionalidadesRepository repository;
+
+    // List
+    public List<Funcionalidades> getAll(){
+        return repository.findAll();
+    }
+
+    // List by UUID
+    public Optional<Funcionalidades> getById(String id){
+        return repository.findById(id);
+    }
+
+    // Create
+    public Funcionalidades save(Funcionalidades entity){
+
+    }
+
+    // Delete
+    public void delete(String id){
+        repository.deleteById(id);
+    }
 }
