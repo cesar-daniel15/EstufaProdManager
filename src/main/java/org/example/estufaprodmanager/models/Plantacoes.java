8d0b0f91f51a336d6cc297a5eb8ce0cbe219af90
Rúben Oliveira Benedito <162583130+RubenBenedito@users.noreply.github.com>
2026-03-11 23:44:20 +0000

    Criação PlantacoesService

--- src/main/java/org/example/estufaprodmanager/models/Plantacoes.java
+++ src/main/java/org/example/estufaprodmanager/models/Plantacoes.java
@@ -12,6 +12,7 @@ import lombok.experimental.SuperBuilder;
 
 import java.io.Serializable;
 import java.util.Date;
+import java.util.UUID;
 
 @Entity
 @Getter
@@ -26,7 +27,7 @@ public class Plantacoes implements Serializable {
 
     @Id
     @Column(name = "id", nullable = false)
-    private String id;
+    private UUID id;
 
     @Column(name = "id_epoca_plantacao", nullable = false)
     private String idEpocaPlantacao;
