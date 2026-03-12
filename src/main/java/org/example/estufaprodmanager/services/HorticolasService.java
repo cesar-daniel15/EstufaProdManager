72e751a977b80e4cad9ef8d584bac08019481d06
Rúben Oliveira Benedito <162583130+RubenBenedito@users.noreply.github.com>
2026-03-12 20:47:07 +0000

    Criação do HorticolasService e correção de erros

---
+++ src/main/java/org/example/estufaprodmanager/services/HorticolasService.java
@@ -0,0 +1,64 @@
+package org.example.estufaprodmanager.services;
+
+import org.example.estufaprodmanager.models.Horticolas;
+import org.example.estufaprodmanager.repositories.HorticolasRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+public class HorticolasService {
+
+    @Autowired
+    private HorticolasRepository repository;
+
+    // List
+    public List<Horticolas> getAll(){
+        return repository.findAll();
+    }
+
+    // List by UUID
+    public Optional<Horticolas> getById(UUID id){
+        return repository.findById(id);
+    }
+
+    // Create
+    public Horticolas save(Horticolas horticola){
+        horticola.setId(UUID.randomUUID());
+        return repository.save(horticola);
+    }
+
+    // Update
+    public Horticolas update(UUID id, Horticolas novaHorticola){
+
+        Optional<Horticolas> horticolaExistente = repository.findById(id);
+
+        if(horticolaExistente.isPresent()){
+
+            Horticolas horticola = horticolaExistente.get();
+
+            horticola.setNome(novaHorticola.getNome());
+            horticola.setDescricao(novaHorticola.getDescricao());
+            horticola.setCategoria(novaHorticola.getCategoria());
+            horticola.setCriadoPor(novaHorticola.getCriadoPor());
+            horticola.setDataAtualizacao(novaHorticola.getDataAtualizacao());
+
+            return repository.save(horticola);
+
+        } else {
+            throw new RuntimeException("Hortícola não encontrada com o ID: " + id);
+        }
+    }
+
+    // Delete
+    public void delete(UUID id){
+        if(repository.existsById(id)){
+            repository.deleteById(id);
+        } else {
+            throw new RuntimeException("Hortícola não encontrada");
+        }
+    }
+}
