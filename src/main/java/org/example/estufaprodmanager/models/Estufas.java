8ba1c2bae9c2e6cb9fdc6466d31d7c7c50a7dbd3
Rúben Oliveira Benedito <162583130+RubenBenedito@users.noreply.github.com>
2026-03-12 21:09:11 +0000

    Criação do EstufasService e atualização dos outros ficheiros

--- src/main/java/org/example/estufaprodmanager/models/Estufas.java
+++ src/main/java/org/example/estufaprodmanager/models/Estufas.java
@@ -12,6 +12,7 @@ import lombok.experimental.SuperBuilder;
 
 import java.io.Serializable;
 import java.util.Date;
+import java.util.UUID;
 
 @Entity
 @Getter
@@ -26,7 +27,7 @@ public class Estufas implements Serializable {
 
     @Id
     @Column(name = "id", nullable = false)
-    private String id;
+    private UUID id;
 
     @Column(name = "identificacao", nullable = false)
     private String identificacao;
