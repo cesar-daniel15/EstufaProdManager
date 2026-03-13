f4b288422f2e2ce43401372437fd7dda9b75d220
cesar-daniel15 <cesardaniel.contacto@gmail.com>
2026-03-13 20:12:56 +0000

    Update main file

--- src/main/java/org/example/estufaprodmanager/EstufaProdManagerApplication.java
+++ src/main/java/org/example/estufaprodmanager/EstufaProdManagerApplication.java
@@ -16,9 +16,6 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.UUID;
-
-
 @SpringBootApplication
 public class EstufaProdManagerApplication implements CommandLineRunner {
 
